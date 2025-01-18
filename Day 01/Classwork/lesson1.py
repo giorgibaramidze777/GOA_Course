66f3d6a90c708b6b14b5e8c39b9e7ecdf7891c94
giorgibaramidze777 <giorgibaramidze762@gmail.com>
2025-01-19 01:34:08 +0400

    Commit

---
+++ Day 01/Classwork/lesson1.py
@@ -0,0 +1,37 @@
+name = "giorgi"
+# name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "nika" არის ცლადისთვის მნიშვნელობა
+
+
+surname = "baramidze"
+
+
+
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+
+
+name = "giorgi" #ეს არის str (string) ტიპის ცვლადი
+age = 10 #ეს არის int ( integer ) მთელი რიცხვი
+height = 146 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+
+knows_programming = True   #True ან False
+is_ugly = False #snakecase (უბრალოდ ცერის სტილი სტანდარტულად)
+isUgly = False #ჯავასკრიპტული camelcase
+
+#print (name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+
+#print (type(age))
+#print (type(name))
+#print (type(surname))
+#print (type(height))
+#print (type(knows_programming))
+
+
+print(name + " " + surname + " " + str(age) + " " + str(height))
