7663438e5404611360bc348fd27352c3f45cd71a
giorgibaramidze777 <giorgibaramidze762@gmail.com>
2025-02-20 20:34:39 +0400

    commit

---
+++ Day 04/classwork/lesson04.py
@@ -0,0 +1,5 @@
+print (name) #str ბრჭყალების გარეშე
+("giorgi") print #ჯერ უნდა იყოს print
+123452 = problem #ცვლადი იწყება ციფრებით
+print (string + 12) #string + integer 
+print error #print ფრჩხილის გარეშე
---
+++ Day 04/homework/hm.py
@@ -0,0 +1,45 @@
+name = "nini"
+name = "nikoloz"
+name = "demetre"
+name = "irakli"
+name = "davit"
+name = "giorgi"
+print(name)
+
+name = "tamar"
+name = "luka"
+name = "ana"
+name = "sandro"
+name = "mariam"
+name = "tamazi"
+print(name)
+
+name = "elene"
+name = "dato"
+name = "salome"
+name = "teona"
+name = "zura"
+name = "nino"
+print(name)
+
+
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+print(num1 + num2 + num3 + num4 + 10)
+
+# 1
+#num1 = ;one; # ; ; მაგივრად უნდა იყოს "" ან ''. num1 = "one" ან 'one'
+#print(num1)
+# 2
+#num2 = ('two') # აქ არ არის print(num2). სწორი არის: print(num2)
+# 3
+#Num3 = 'three'
+#print(num3) # აქ არის არასწორად დაწერილი არის ცვლადის სახელი. სწორი არის: print(Num3)
+# 4
+#num4 = 'number4" ' # აქ არასწორია ის რომ ჯერ ' და მერე " უნდა იყოს '' ან "". სწორი არის: num4 = 'number4' ან num4 = "number4"
+#print(num4)
+# 5
+#5num = 'five' # აქ არასწორია ის რომ ცვლადის სახელი არ შეიძლება დაიწყოს რიცხვით. სწორი არის: num5 = 'five'
+#print(5num)
