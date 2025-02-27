938c1a63d8556b626839680fdcf499ef3f0d64f0
giorgibaramidze777 <giorgibaramidze762@gmail.com>
2025-02-27 15:22:04 +0400

    Commit

---
+++ Day 06/classwork/lesson06.py
@@ -0,0 +1,70 @@
+#1)კოდის დაწერა
+#2)კოდის გაშვება
+#3)კოდის გასწორება(debugging)
+#syntax --> წერის სტილი
+
+#print(hello world") --> error
+
+#case-sensitive
+#name = "gabriel"
+
+#name = "gio"
+
+#name = "gurami"
+
+
+#snake_case
+#snake = გველი
+
+#goa_best = "goa"
+
+#group_62_is_the_best = "best"
+
+
+#camelCase
+#camel = "აქლემს"
+
+#goaBest = "best"
+
+#group62IsTheBest = "best"
+
+
+
+#1) კომენტარებით ახსენი რა არის case-sensitive და snake-case, შემდეგ ორივეზე დაწერე 3 მაგალითი, ასევე გაასწორეთ ეს კოდები:
+#1)
+Name = 'nika'
+print(Name) 
+#2)
+goa_best = 100 #or goaBest = 100
+print(goa_best)
+#3)
+name = 'mate'
+print(name)
+#4)
+number = 10
+print(number)
+#5)
+goabest = "100%"
+print(goabest)
+
+#snake case არის _ გამოყენება მაგ: goa_best = "best"
+#camelCase არის როცა გვინდა რომ 2 სიტყვა იყოს ერთად ანუ მეორე სიტყვა იწერება დიდი ასოთი მაგ: goaBest = "best"
+#case-sensitive არის როცა Goabest და goabest არის სხვა და სხვა ცვლადი ამიტომ არ უნდა შეგვეშალოს როცა ვპრინტავთ მაგ: Goabest = 100 goabest = "200"
+#snake case:goa_best, best_goa, hi_world
+#camelCase: camelCase, caseCamel, goaBest
+#case-sensitive:Goabest = 100, goabest = 200 bestGoa = 300
+
+#input() = შეყვანა
+
+#output = გამოყვანა/გამომავალი
+
+#name = input("What is your name?")
+
+#age = input("What is your age?")
+#print(name)
+
+age1 = int(input("What is your age?"))
+
+age2 = int(input("What is your age?"))
+
+print(age1 + age2)
---
+++ Day 06/homework/hm.py
@@ -0,0 +1,26 @@
+age = input("what is your age?")
+surname = input("what is your surname?")
+name = input("what is your name?")
+date = input("what is your date of birth?")
+print(age)
+print(surname)
+print(name)
+print(date)
+#snake case არის _ გამოყენება მაგ: programming = "i_love_programming"
+#camelCase არის როცა გვინდა რომ 2 სიტყვა იყოს ერთად ანუ მეორე სიტყვა იწერება დიდი ასოთი მაგ: appleFruit = "fruit"
+#case-sensitive არის როცა Bestacademyever და bestacademyever არის სხვა და სხვა ცვლადი ამიტომ არ უნდა შეგვეშალოს როცა ვპრინტავთ მაგ: Bestacademyever = 100 bestacademyever = "200"
+#debuging არის კოდის შესწორება რაც ახლა უნდა გავაკეთო მაგ: goabest < best --> goabest = best ეს არის debuging
+name = 'zura'
+print(name)
+#2)
+age - 13
+print(age)
+#3)
+Hello_world = "Hello" #or helloWorld = "Hello"
+print(Hello_world)
+#4)
+name2 = 'nika'
+print(name2)
+#5)
+year = 2025
+print(year)
