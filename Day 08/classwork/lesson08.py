a0c1caa56ba2c3d868acf96065fc9bbf6629c02d
giorgibaramidze777 <giorgibaramidze762@gmail.com>
2025-03-05 21:12:05 +0400

    Commit

---
+++ Day 08/classwork/lesson08.py
@@ -0,0 +1,70 @@
+#age = input("please enter your age")
+#print("the user's  age is: ",age)
+
+#surname = input("enter your surname: ")
+#print("the user's surname is: ",surname)
+
+#name = input("please enter your name")
+#print("the user's name is: ",name)
+
+#date = input("enter your birth date: ")
+#print("the user's birth date is: ",date)
+
+#num1 = "14"
+#num2 = 15
+
+
+
+# type() --> ამოწმებს მონაცემის ტიპს
+
+# print(num1 + num2)
+
+#type(num1)
+#type(num2)
+
+#print(type(False))
+
+# string --> str
+# integer --> int
+# float --> float
+# Boolean --> bool
+
+#num1 = 30
+#num2 = 15
+#division = num1 / num2
+#print(type(division))
+
+#type() გვეხმარება რომ გავიგოთ რა ტიპია str, int, float, თუ bool
+
+num1 = 10
+str = "string"
+float = 0.7
+bool = True
+num2 = 52
+print(num1)
+print(str)
+print(float)
+print(bool)
+print(num2)
+
+num3 = 16
+num4 = 93
+
+print(num4 / num3)
+print(num4 * num3)
+print(num4 + num3)
+print(num4 - num3)
+
+print(num3 * num4)
+print(num3 / num4)
+print(num3 - num4)
+print(num3 + num4)
+
+print(5.8125)
+print(1488)
+print(109)
+print(77)
+print(1488)
+print(0.17204301075268819)
+print(-77)
+print(109)
---
+++ Day 08/homework/hm08.py
@@ -0,0 +1,15 @@
+# string str = "hi"
+# integer int = 10
+# float flt = 0.8
+# Bolean bool = False
+
+num1 = 19
+num2 = 59
+print(num1 + num2)
+
+Hello = "gamarjoba"
+you = "shen"
+what = "ra"
+name = "gqvia?"
+
+print(Hello + " " + you + " " + what + " " + name)
