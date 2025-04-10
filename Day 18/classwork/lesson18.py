462900b310a4505b4d5d2cb62d81706313ceaff0
giorgibaramidze777 <giorgibaramidze762@gmail.com>
2025-04-10 20:16:14 +0400

    Commit

---
+++ Day 18/classwork/lesson18.py
@@ -0,0 +1,13 @@
+#for i in range(2,10 + 1, 2):
+   # print(i, "hello world")
+
+#for i in range(1, 10, 2):
+    #print(i, "hello world")
+
+#for goa in range(100):
+    #print(goa, "GOA Best!")
+
+
+for i in range (10):
+    x = int(input("Enter a number: "))
+    print(i)
---
+++ Day 18/homework/hm18.py
@@ -0,0 +1,6 @@
+for i in range(1,1001):
+    print(i, "GOA BEST")
+
+Y = int(input("Enter number: "))
+for x in range (Y):
+    print("GOA best")
