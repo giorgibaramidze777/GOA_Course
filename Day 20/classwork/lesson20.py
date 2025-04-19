967c302df86517a0af854ddfaf70db7bf1a8aba7
giorgibaramidze777 <giorgibaramidze762@gmail.com>
2025-04-19 17:32:32 +0400

    Commit

---
+++ Day 20/classwork/lesson20.py
@@ -0,0 +1,18 @@
+# while loop
+
+
+# counter = 0
+# while counter < 5:
+    # print("hello")
+    # counter += 3 # counter = counter + 1
+
+# goa = 0 
+# while goa < 100:
+    # print("Goa best! and best group is group 62")
+    # goa += 1
+
+i = 100
+while i >= 0:
+    print(i)
+    i -= 1
+ 
---
+++ Day 20/homework/hm20.py
@@ -0,0 +1,24 @@
+i = 0
+while i < 3:
+    print("hello")
+    i += 1
+
+a = 1
+while a <= 3:
+    print(a)
+    a += 1
+
+a = input("ჩაწერეთ კარგი: ")
+if a == "კარგი" or a == "kargi":
+    print("თქვენ ჩაწერეთ კარგი!")
+else:
+    print("თქვენ არ ჩაწერეთ კარგი!")
+
+num = 1
+
+while num <= 5:
+    print(num)
+    num += 1
+
+
+# while loop-ს ვიყენებთ რომ დავწეროთ რამე იმდენჯერ სანამ არ მიახწევს იმ ციფრს რაც გვინდა
