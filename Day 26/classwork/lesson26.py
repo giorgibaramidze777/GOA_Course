462f68d29ba0da26689666703667c7a6939bbb35
giorgibaramidze777 <giorgibaramidze762@gmail.com>
2025-05-12 12:45:52 +0400

    commit

---
+++ Day 26/classwork/lesson26.py
@@ -0,0 +1,31 @@
+# if else,
+
+# user = int(input("Enter a number: "))
+
+# if user >= 10:
+    # print("number is bigger than guramis iq ")
+# elif user == 5:
+    # print("number is 5")
+# elif user == 6:
+    # print("number is 6")
+# else:
+    # print("number is smaller than guramis iq")
+
+n1 = int(input("Enter a number: "))
+
+if n1 == 7:
+    print("number is 7")
+elif n1 == 3:
+    print("number is 3")
+else:
+    print("number is not 7 or 3")
+
+user = int(input("Enter a number: "))
+if user >= 10:
+    if user == 15:
+        print("number is 15")
+    elif user == 12:
+        print("number is 12")
+    else:
+        print("number is bigger than 10 but not 12 or 15")
+
---
+++ Day 26/homework/hm26.py
@@ -0,0 +1,34 @@
+user = int(input("Enter your age: "))
+if user >= 18:
+    print("You are an adult.")
+elif user == 15:
+    print("You are teenager.")
+elif user == 16:
+    print("You are teenager.")
+elif user == 17:
+    print("You are teenager.")
+else:
+    print("You are a child.")
+
+
+name = input("Enter your name on english: ")
+if name == "Gurami" or name == "gurami":
+    print("YOU ARE NOT MENTOR")
+elif name == "Aleqsandre" or name == "aleqsandre":
+    print("YOU ARE MENTOR")
+else:
+    print("YOU ARE GOA STUDENT")
+
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+if num2 > num1:
+    print("Level up")
+elif num1 > num2:
+    print("Level down")
+else:
+    print("Level updown")
+
+# if - თუ - თუ შესრულდა იფ მერე ელიფ და ელზი არ შესრულდება - განსხვავდება ყველა იმით ცოტა სხვა შემთხვევაში ვიყენებთ
+# else - სხვა/სხვა შემთხვევაში - აქ მე მგონი ახსნაც არ უნდა მაგრამ თუ იფ და ელიფ არ შესრულდა შესრულდება else ანუ ეს არის სხვა შემთხვევაში
+# elif - იგივე if - ელიფი შეგიძლია დაწერო სანამ კომპიუტერი არ მოკდება
