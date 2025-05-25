adf21dd1b0664d63e11597867619401804fbbddf
giorgibaramidze777 <giorgibaramidze762@gmail.com>
2025-05-25 19:27:18 +0400

    commit

---
+++ Day 30/classwork/lesson30.py
@@ -0,0 +1,19 @@
+listn = [1, 2, 3, 4, 5]
+
+inp = int(input("Enter a number: "))
+if inp <= 5:
+    print(listn[inp])
+else:
+    print("გურამი error")
+
+
+fruit = ["apple", "banana", "cherry", "kiwi", "mango", "watermelon"]
+print(fruit[0:3])
+
+print(fruit[:4])
+
+print(fruit[3:])
+
+print(fruit[::2])
+
+print(fruit[::-1])
---
+++ Day 30/homework/hm30.py
@@ -0,0 +1,21 @@
+# 1
+listn = [1, 2, 3, 4, 5]
+inp = int(input("Enter a number: "))
+if inp <= 5:
+    print(listn[inp])
+else:
+    print("გურამი error")
+
+# 2
+# index == listn = [1, 2, 3, 4, 5] 1 == 0[index] 2 == 1[index] 3 == 2[index] 4 == 3[index] 5 == 4[index]
+
+# 3
+listg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print(listg[0:10])
+
+# 4
+listj = ["apple", "banana", "cherry", "mango", "watermelon"]
+print(listj[2:4])
+
+# 5
+# sololearn
